64fb23ca524ca2027a80f0254c761b1fa7df0c21
Alexander Konukhov <jesusmisha@gmail.com>
2022-12-24 16:40:08 +0300

    start point: logging don't work

--- app.py
+++ app.py
@@ -1,15 +1,19 @@
-import asyncio
-from loader import bot, logging
+# from loader import bot, logging
+import logging
+
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, CommandHandler
+from TOKEN import BOT_TOKEN
 
 
-async def main():
-    async with bot:
-        print(await bot.get_me())
-        print((await bot.get_updates())[0])
-        await bot.send_message(text='Hi John!', chat_id=601278313)
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
 
 
 if __name__ == '__main__':
-    asyncio.run(main())
+    application = ApplicationBuilder().token(BOT_TOKEN).build()
+
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+
+    application.run_polling()
